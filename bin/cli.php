17e93a84552b5a6cea53fc2d82442cf26294ee65
James Titcumb <james@asgrim.com>
2026-03-23 14:49:54 +0000

    initial very basic CLI interface for verify

---
+++ bin/cli.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Console\Application;
+
+// phpcs:ignore Squiz.NamingConventions.ValidVariableName.NotCamelCaps
+include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
+
+$application = new Application('Attestation CLI');
+$application->addCommands([
+    new \ThePhpFoundation\Attestation\Command\Verify(),
+]);
+
+$application->run();
