76b8601e4df1b5dfe9d966faf600bc4a897cda42
James Titcumb <james@asgrim.com>
2026-08-26 21:09:51 +0100

    Add VO for InclusionProof

---
+++ src/InclusionProof.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThePhpFoundation\Attestation;
+
+use Webmozart\Assert\Assert;
+
+use function array_key_exists;
+use function base64_decode;
+
+/** @internal This is not a public API, so should not be depended upon unless you accept the risk of BC breaks */
+final class InclusionProof
+{
+    public int $logIndex;
+    public string $rootHash;
+    public int $treeSize;
+    /** @var list<string> */
+    public array $hashes;
+    public ?string $checkpointEnvelope;
+
+    /** @param list<string> $hashes */
+    private function __construct(int $logIndex, string $rootHash, int $treeSize, array $hashes, ?string $checkpointEnvelope)
+    {
+        $this->logIndex           = $logIndex;
+        $this->rootHash           = $rootHash;
+        $this->treeSize           = $treeSize;
+        $this->hashes             = $hashes;
+        $this->checkpointEnvelope = $checkpointEnvelope;
+    }
+
+    /** @param array<array-key, mixed> $inclusionProof */
+    public static function fromBundleInclusionProof(array $inclusionProof): self
+    {
+        Assert::keyExists($inclusionProof, 'logIndex');
+        Assert::stringNotEmpty($inclusionProof['logIndex']);
+        Assert::numeric($inclusionProof['logIndex']);
+
+        Assert::keyExists($inclusionProof, 'rootHash');
+        Assert::stringNotEmpty($inclusionProof['rootHash']);
+        $rootHash = base64_decode($inclusionProof['rootHash']);
+        Assert::stringNotEmpty($rootHash);
+
+        Assert::keyExists($inclusionProof, 'treeSize');
+        Assert::stringNotEmpty($inclusionProof['treeSize']);
+        Assert::numeric($inclusionProof['treeSize']);
+
+        Assert::keyExists($inclusionProof, 'hashes');
+        Assert::isArray($inclusionProof['hashes']);
+        $hashes = [];
+        foreach ($inclusionProof['hashes'] as $hash) {
+            Assert::stringNotEmpty($hash);
+            $decodedHash = base64_decode($hash);
+            Assert::stringNotEmpty($decodedHash);
+            $hashes[] = $decodedHash;
+        }
+
+        $checkpointEnvelope = null;
+        if (array_key_exists('checkpoint', $inclusionProof)) {
+            Assert::isArray($inclusionProof['checkpoint']);
+            if (array_key_exists('envelope', $inclusionProof['checkpoint'])) {
+                Assert::stringNotEmpty($inclusionProof['checkpoint']['envelope']);
+                $checkpointEnvelope = $inclusionProof['checkpoint']['envelope'];
+            }
+        }
+
+        return new self(
+            (int) $inclusionProof['logIndex'],
+            $rootHash,
+            (int) $inclusionProof['treeSize'],
+            $hashes,
+            $checkpointEnvelope,
+        );
+    }
+}
---
+++ test/unit/InclusionProofTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThePhpFoundation\UnitTest\Attestation;
+
+use PHPUnit\Framework\TestCase;
+use ThePhpFoundation\Attestation\InclusionProof;
+
+use function base64_decode;
+
+/** @covers \ThePhpFoundation\Attestation\InclusionProof */
+final class InclusionProofTest extends TestCase
+{
+    public function testFromBundleInclusionProof(): void
+    {
+        $inclusionProof = InclusionProof::fromBundleInclusionProof([
+            'logIndex' => '339175637',
+            'rootHash' => 'AhbZoVyv2FfnyQjQoGoj8crpY4R1tWuJiLVIUJxmGB4=',
+            'treeSize' => '339175655',
+            'hashes' => [
+                '+cZUU5T9fPeBM0sh9Tr6pkevLIqAm2g7wQjSeibo4Vg=',
+                'h7VJtejEapYJ0lVmwmrZd8GFeWKSibdiJmP+aLD8/kc=',
+            ],
+            'checkpoint' => ['envelope' => "rekor.sigstore.dev - 1193050959916656506\n339175655\nAhbZoVyv2FfnyQjQoGoj8crpY4R1tWuJiLVIUJxmGB4=\n\n— rekor.sigstore.dev wNI9ajBGAiEAhEsbDtU0G6tkfpxcTPi5Q0Tsi/Z85DBcGst+iLS6NncCIQDPlvrm+OVQyT31gOp90Mh74np6cFEryT2ybYVWq7CNRw==\n"],
+        ]);
+
+        self::assertSame(339175637, $inclusionProof->logIndex);
+        self::assertSame(base64_decode('AhbZoVyv2FfnyQjQoGoj8crpY4R1tWuJiLVIUJxmGB4='), $inclusionProof->rootHash);
+        self::assertSame(339175655, $inclusionProof->treeSize);
+        self::assertCount(2, $inclusionProof->hashes);
+        self::assertSame(base64_decode('+cZUU5T9fPeBM0sh9Tr6pkevLIqAm2g7wQjSeibo4Vg='), $inclusionProof->hashes[0]);
+        self::assertSame(base64_decode('h7VJtejEapYJ0lVmwmrZd8GFeWKSibdiJmP+aLD8/kc='), $inclusionProof->hashes[1]);
+        self::assertSame(
+            "rekor.sigstore.dev - 1193050959916656506\n339175655\nAhbZoVyv2FfnyQjQoGoj8crpY4R1tWuJiLVIUJxmGB4=\n\n— rekor.sigstore.dev wNI9ajBGAiEAhEsbDtU0G6tkfpxcTPi5Q0Tsi/Z85DBcGst+iLS6NncCIQDPlvrm+OVQyT31gOp90Mh74np6cFEryT2ybYVWq7CNRw==\n",
+            $inclusionProof->checkpointEnvelope,
+        );
+    }
+
+    public function testFromBundleInclusionProofWithoutCheckpoint(): void
+    {
+        $inclusionProof = InclusionProof::fromBundleInclusionProof([
+            'logIndex' => '1',
+            'rootHash' => 'AhbZoVyv2FfnyQjQoGoj8crpY4R1tWuJiLVIUJxmGB4=',
+            'treeSize' => '2',
+            'hashes' => [],
+        ]);
+
+        self::assertNull($inclusionProof->checkpointEnvelope);
+        self::assertSame([], $inclusionProof->hashes);
+    }
+}
